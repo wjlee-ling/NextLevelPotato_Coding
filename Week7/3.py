1542d371d9e3b35aec29f2d5978fabdee3804b3c
JEONGAH LEE <65378914+jjeongah@users.noreply.github.com>
2022-11-22 08:24:00 +0900

    visited

--- Week7/3.py
+++ Week7/3.py
@@ -4,37 +4,47 @@
 K번째 섬에서 K번째 섬으로 오는 최소한의 거리 (하나 이상의 다른 섬) / 못 돌아오면 -1
 
 idea: 최단 경로를 찾는 bfs
-*** 시간 초과
+visited를 True, False가 아닌 cn로!!
 '''
 
+#import sys
+#sys.stdin = open('./quiz/input2.txt', 'r')
+
 from collections import deque
-import sys
-sys.stdin = open('./quiz/input2.txt', 'r')
 
-def BFS(v, cnt):
-    queue = deque([v]) # 큐(Queue) 구현을 위해 deque 라이브러리 사용
-    visited[v] = True
-    while queue: # 큐가 빌 때까지
-        v = queue.popleft() # 큐에서 하나의 원소를 뽑아 출력
-        print(v)
-
-        # 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
-        for i in graph[v]:
-            if not visited[i]:
-                cnt +=1 
-                queue.append(i)
-                visited[i] = True
-
-N, M, K = map(int, input().split())
-graph = [[] for _ in range(N+1)]
-
-for _ in range(M):
+# 1. 입력 받기
+n, m, k = map(int, input().split())
+'''
+graph = defaultdict(list)
+for i in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
-print(graph)
+'''
+graph = [[] for _ in range(n+1)]
 
-visited = [False]*(M+1)
-cnt = 0
-BFS(1, cnt)
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+#print(graph)
 
+# 2. BFS
+q = deque()
+visited = [ 0 for _ in range(n+1)]
 
+# 초기 상태 지정
+q.append(k)
+visited[k] = 0
+
+while q:
+    v = q.popleft()
+    for i in graph[v]:
+        if visited[i] == 0:
+            visited[i] = visited[v] + 1
+            q.append(i)
+        if i == k:
+            break
+
+if visited[k] == 0:
+    print(-1)
+else:
+    print(visited[k])
