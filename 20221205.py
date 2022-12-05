0b975e2ac8605a8c0b71d220008a42686cb17e91
lim4349 <rockmanzero@naver.com>
2022-12-05 13:57:23 +0900

    최댓값과 최소값

---
+++ 20221205.py
@@ -0,0 +1,6 @@
+def solution(s):
+        answer = ''
+            arr = list(map(int, s.split()))
+                answer = str(min(arr)) + " " + str(max(arr))
+                    
+                        return answer
