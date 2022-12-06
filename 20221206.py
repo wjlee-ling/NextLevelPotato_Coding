6d6c28c4995a390e8895c28298ecca82256350b4
Zero <rockmanzero@naver.com>
2022-12-06 15:47:58 +0900

    Update 20221206.py

--- 20221206.py
+++ 20221206.py
@@ -1,12 +1,11 @@
-olution(s):
-        words = list(s.lower())
-            
-                answer = ''
-                    for i in range(len(words)):
-                                if words[i] == ' ' and i < len(words)-1:
-                                                words[i+1] = words[i+1].upper()
-                                                        elif i == 0:
-                                                                        words[i] = words[i].upper()
-                                                                                answer = ''.join(words)
-                                                                                    
-                                                                                        return answer
+def solution(s):
+    words = list(s.lower())
+    answer = ''
+    for i in range(len(words)):
+        if words[i] == ' ' and i < len(words)-1:
+            words[i+1] = words[i+1].upper()
+        elif i == 0:
+            words[i] = words[i].upper()
+            answer = ''.join(words)
+
+    return answer
