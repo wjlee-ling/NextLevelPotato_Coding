2f358bcae88e5a5ceeeaabd7f5d69179fbd23652
Wonjae LEE <wjlee.ling@gmail.com>
2022-12-08 08:23:07 +0900

    Update 20221208.py

--- 20221208.py
+++ 20221208.py
@@ -1,5 +1,32 @@
 def solution(n, roads, sources, destination):
     from collections import deque
+    # try 2: fix 1st try's minimum distance error
+    links = dict()
+    for a, b in roads:
+        links[a] = links.get(a, []) + [b] 
+        links[b] = links.get(b, []) + [a]
+        
+    queue = deque([destination])
+    dists = [100001] *(n+1)
+    dists[destination] = 0
+    visited = [0]*(n+1)
+    
+    while queue:
+        curr = queue.popleft()
+        visited[curr] = 1
+        if links.get(curr, None):
+            for node in links[curr]:
+                if visited[node] == 0:
+                    queue.append(node)
+                    dists[node] = min(dists[node], dists[curr] +1)
+    answer = []
+    for s in sources:
+        if s == destination:
+            answer.append(0)
+        elif dists[s] == 100001:
+            answer.append(-1)
+        else:
+            answer.append(dists[s])    
     
     # try 1: dictionary => failure: 6~ 15
     # shortest => bfs starting from destination
