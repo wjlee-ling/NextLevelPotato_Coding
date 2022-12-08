bef43f4acd5c4151b0dc2158104eff9f2fea4ff8
ezez-refer <jjn00712@gmail.com>
2022-12-08 23:11:19 +0900

    study

---
+++ 20221209.py
@@ -0,0 +1,18 @@
+import math
+
+
+def solution(k, d):
+    answer = 0
+
+    for x in range(0, d + 1, k):
+        y = int(math.sqrt(d**2 - x**2))
+        answer += y // k + 1
+
+    return answer
+
+
+if __name__ == "__main__":
+    k = [2, 1]
+    d = [4, 5]
+    for n1, n2 in zip(k, d):
+        print(solution(n1, n2))
