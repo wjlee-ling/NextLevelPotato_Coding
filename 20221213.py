71bb0928e739618858f121f94990ad3b00e3a457
Wonjae LEE <wjlee.ling@gmail.com>
2022-12-14 16:22:26 +0900

    Create 20221213.py

---
+++ 20221213.py
@@ -0,0 +1,11 @@
+def solution(stones, k):
+    # # first try -> fails efficiency only
+    # answer = 0
+    # diffs = []
+    # min_diff = 200000000
+    # for idx in range(len(stones)-k+1):
+    #     window = stones[idx:idx+k]
+    #     diff = max(window)-min(window)
+    #     diffs.append(max(window))
+    # answer = min(diffs)    
+    return answer
