4dfe4f7a8b21757f739ea3b2aa03a0b950c14bb5
wjlee-ling <wjlee.ling@gmail.com>
2023-04-14 09:50:43 +0900

    fix: get boj_ 14503 right

--- BOJ_14503.py
+++ BOJ_14503.py
@@ -6,7 +6,6 @@ graph = []
 for _ in range(n):
     row = list(map(int, sys.stdin.readline().split()))
     graph.append(row)
-
 moves = [(-1, 0), (0,1), (1,0), (0,-1)] # move when heading N, E, S, W, respectively
 q = deque([(r,c)]) # start
 
@@ -14,8 +13,10 @@ def look_around(x,y):
     # 청소할 공간이 있으면 True
     for dx, dy in moves:
         nx, ny = x+dx, y+dy
-        if valid(nx,ny) and (nx,ny) not in cleaned:
-            # 벽이 아니고 청소 필요
+        if not valid(nx,ny):
+            continue
+        if graph[nx][ny] == 0 and (nx,ny) not in cleaned:
+            # 벽이 없고 청소할 수 있음
             return True
     return False
 
@@ -23,30 +24,32 @@ def valid(x,y):
     if x < 0 or y < 0 or x>=n or y>=m:
         return False
     if graph[x][y] == 1:
-        # 벽
+        # 벽이 있음
         return False
     return True
 
 def change_heading(heading):
     # N -> W (0->3), S -> E (2 ->1)
-    return heading-1 if (heading>=1) else 3
+    new = heading-1 if (heading>=1) else 3
+    return new
 
 ans = 0
 cleaned = set()
 while q:
     x,y = q.popleft()
-    if (x,y) not in cleaned:
+    if graph[x][y] == 0 and (x,y) not in cleaned:
         ans+=1
-        cleaned.add((x,y)) # cleaned
+        cleaned.add((x,y)) # visited
 
     if look_around(x,y):
         # 청소할 면이 있으면
-        d = change_heading(d) # 방향 반시계 구십도
-        dx, dy = moves[d]
-        nx, ny = x+dx, y+dy
-        if valid(nx,ny) and (nx,ny) not in cleaned: # 청소할 수 있음
-            q.append((nx,ny))
-        
+        for _ in range(4):
+            d = change_heading(d) # 방향 반시계 구십도
+            dx, dy = moves[d]
+            nx, ny = x+dx, y+dy
+            if valid(nx,ny) and (nx,ny) not in cleaned: # 청소할 수 있음
+                q.append((nx,ny))
+                break
     else:
         # 청소 없음
         # 방향 유지, 한칸 후진
