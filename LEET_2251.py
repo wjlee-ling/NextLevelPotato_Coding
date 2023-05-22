65ef352d58f93dc7e176ac5e51eb8cb30a71cfea
JN-Jeong <jjn00712@gmail.com>
2023-05-22 10:41:01 +0900

    study

--- LEET_2251.py
+++ LEET_2251.py
@@ -26,5 +26,5 @@ if __name__ == "__main__":
 
 """
 bisect_right(data: list, target: int) : 길이가 n이며 원소 값이 숫자이고 정렬된 list(data)의 n-1번째 index부터 차례대로 target 값이 위치할 index를 찾음
-bisect_right(data: list, target: int) : 길이가 n이며 원소 값이 숫자이고 정렬된 list(data)의 0번째 index부터 차례대로 target 값이 위치할 index를 찾음
+bisect_left(data: list, target: int) : 길이가 n이며 원소 값이 숫자이고 정렬된 list(data)의 0번째 index부터 차례대로 target 값이 위치할 index를 찾음
 """
