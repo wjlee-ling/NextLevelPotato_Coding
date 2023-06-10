4ae34dd767b7e0fe95980b23986131ca3f0b56ab
JN-Jeong <jjn00712@gmail.com>
2023-06-10 11:28:31 +0900

    study

--- BOJ_14500.py
+++ BOJ_14500.py
@@ -4,68 +4,52 @@
 
 
 def solution():
-    # fmt:off
-    tetrominos = [[[1, 1, 1, 1]], 
-                  
-                  [[1], 
-                   [1], 
-                   [1], 
-                   [1]],
-                  
-                  [[1, 1], 
-                   [1, 1]], 
-                  
-                  [[1, 0], 
-                   [1, 0], 
-                   [1, 1]], 
-                  
-                  [[1, 0], 
-                   [1, 1], 
-                   [0, 1]], 
-                  
-                  [[1, 1, 1], 
-                   [0, 1, 0]]]
-    # fmt:on
-
-    def rot(shape, n):
-        temp = shape[:]
-        for i in range(n):
-            temp = list(reversed(list(map(list, zip(*temp)))))
-        return temp
-
-    def symmetry(shape):
-        return list(map(list, zip(*shape)))
-
-    for i in range(3, 6):
-        temp = tetrominos[i]
-        tetrominos.append(symmetry(temp))
-        for n in range(1, 4):
-            temp = rot(temp, n)
-            if temp not in tetrominos:
-                tetrominos.append(temp)
-            if symmetry(temp) not in tetrominos:
-                tetrominos.append(symmetry(temp))
-
-    # for t in tetrominos:
-    #     print(t)
-    # print()
-
     N, M = map(int, input().split())
     board = [list(map(int, input().split())) for _ in range(N)]
+    global answer
     answer = 0
+    max_val = max(map(max, board))
 
-    for r in range(N):
-        for c in range(M):
-            for t in tetrominos:
-                result = 0
-                for i in range(len(t)):
-                    for j in range(len(t[0])):
-                        if 0 <= r + i < N and 0 <= c + j < M and t[i][j] == 1:
-                            result += board[r + i][c + j]
-                answer = max(answer, result)
+    dy = [-1, 1, 0, 0]
+    dx = [0, 0, -1, 1]
 
-    print(answer)
+    visited = [[0] * M for _ in range(N)]
+
+    def is_valid(y, x):
+        if 0 <= y < N and 0 <= x < M:
+            return True
+        return False
+
+    def dfs(y, x, cnt, total):
+        global answer
+        if answer >= total + max_val * (4 - cnt):
+            return
+
+        if cnt == 4:
+            answer = max(answer, total)
+            return
+
+        for d in range(4):
+            ny = y + dy[d]
+            nx = x + dx[d]
+
+            if is_valid(ny, nx) and visited[ny][nx] == 0:
+                visited[ny][nx] = 1
+                dfs(ny, nx, cnt + 1, total + board[ny][nx])
+                visited[ny][nx] = 0
+                if cnt == 2:  # ㅗ, ㅜ, ㅓ, ㅏ 모양
+                    visited[ny][nx] = 1
+                    dfs(y, x, cnt + 1, total + board[ny][nx])
+                    visited[ny][nx] = 0
+
+    for i in range(N):
+        for j in range(M):
+            visited[i][j] = 1
+            dfs(i, j, 1, board[i][j])
+            visited[i][j] = 0
+
+    return answer
 
 
 if __name__ == "__main__":
-    solution()
+    print(solution())
