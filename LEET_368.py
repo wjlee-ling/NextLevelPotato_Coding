0db3187c1fdd8f56a463fe0bff9872845deccb39
wjlee-ling <wjlee.ling@gmail.com>
2023-06-16 07:58:58 +0900

    [update] leet_368 success

--- LEET_368.py
+++ LEET_368.py
@@ -1,24 +1,23 @@
 # https://leetcode.com/problems/largest-divisible-subset/
+from typing import List
 
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
-        ## 새로운 num이 있을 때 이전 num 중 가장 공약수가 많은 숫자 선택
+        ## 새로운 num이 있을 때 이전 num 중 가장 큰 num
         d = dict() # {가장 작은 수: 배수}
         nums.sort()
         for num in nums:
-            flag = 0
-            for k in d.keys():
-                if num % k == 0 and k != 1:
-                    d[k].append(num)
-                    flag = 1
-                elif k % num == 0:
-                    d[num].append(k)
-                    flag = 1
-            if flag == 0:
-                ## 기존에 있는 것과 안됨
-                d[num] = [num]
-                if nums[0] == 1 and num !=1:
-                    d[num].append(1)
-        print(d)
+            d[num] = []
+            if num == 1:
+                d[num] = [1]
+                continue
+            for key in d.keys():
+                if key != num and num % key == 0:
+                    if len(d[key]) > len(d[num]):
+                        d[num] = d[key].copy() # d[key] 하면 동기화됨
+                        
+            d[num].append(num)
+
+
         ls = sorted(d.values(), key = len)
         return ls[-1]
