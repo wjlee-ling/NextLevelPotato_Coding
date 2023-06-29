641df2e5f342009dddb0ee5c0cdfa58a545030a6
JN-Jeong <jjn00712@gmail.com>
2023-06-29 14:55:30 +0900

    study

--- BOJ_15683.py
+++ BOJ_15683.py
@@ -1,5 +1,5 @@
 """
-감시
+15683. 감시
 """
 
 
@@ -7,63 +7,66 @@ def solution():
     N, M = map(int, input().split())
     board = [list(map(int, input().split())) for _ in range(N)]
     global answer
-    answer = N * M
+    answer = float("inf")
 
-    cctvs = []
-    for i in range(N):
-        for j in range(M):
-            if 1 <= board[i][j] <= 5:
-                cctvs.append((board[i][j], i, j))
+    # 상좌하우
+    dy = [-1, 0, 1, 0]
+    dx = [0, -1, 0, 1]
 
-    dy = [-1, 1, 0, 0]
-    dx = [0, 0, -1, 1]
-
-    mode = [
+    modes = [
         [],
         [[0], [1], [2], [3]],
-        [[0, 1], [2, 3]],
-        [[0, 3], [1, 3], [1, 2], [0, 2]],
-        [[0, 2, 3], [0, 1, 3], [1, 2, 3], [0, 1, 2]],
+        [[0, 2], [1, 3]],
+        [[0, 1], [1, 2], [2, 3], [3, 0]],
+        [[0, 1, 2], [1, 2, 3], [2, 3, 0], [3, 0, 1]],
         [[0, 1, 2, 3]],
     ]
 
-    def watch(board, mode, row, col):
+    def check(board):
+        result = 0
+        for i in range(N):
+            for j in range(M):
+                if board[i][j] == 0:
+                    result += 1
+
+        return result
+
+    def is_valid(y, x):
+        if 0 <= y < N and 0 <= x < M:
+            return True
+        return False
+
+    cctvs = []
+    for y in range(N):
+        for x in range(M):
+            if 1 <= board[y][x] <= 5:
+                cctvs.append([board[y][x], y, x])
+
+    def watch(board, mode, y, x):
         for d in mode:
-            nrow = row
-            ncol = col
-
+            ny = y
+            nx = x
             while True:
-                nrow += dy[d]
-                ncol += dx[d]
-                if nrow < 0 or nrow >= N or ncol < 0 or ncol >= M:
+                ny += dy[d]
+                nx += dx[d]
+
+                if not is_valid(ny, nx) or board[ny][nx] == 6:
                     break
 
-                if board[nrow][ncol] == 6:
-                    break
+                board[ny][nx] = 7
 
-                if board[nrow][ncol] == 0:
-                    board[nrow][ncol] = 7
-
-    def dfs(cnt, arr):
+    def dfs(cnt, board):
         global answer
-
         if cnt == len(cctvs):
-            result = 0
-            for i in range(N):
-                result += arr[i].count(0)
-            # if answer > result:
-            #     for a in arr:
-            #         print(a)
-            #     print()
-            answer = min(answer, result)
+            answer = min(answer, check(board))
             return
 
-        temp = [a[:] for a in arr]
-        cctv_num, row, col = cctvs[cnt]
-        for m in mode[cctv_num]:
-            watch(temp, m, row, col)
+        temp = [b[:] for b in board]
+        cctv_num, y, x = cctvs[cnt]
+        for mode in modes[cctv_num]:
+            watch(temp, mode, y, x)
             dfs(cnt + 1, temp)
-            temp = [a[:] for a in arr]
+            temp = [b[:] for b in board]
 
     dfs(0, board)
     print(answer)
